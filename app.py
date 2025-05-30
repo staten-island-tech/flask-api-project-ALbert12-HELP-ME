5c52defe638f2a2ae88d64ba3fc4c86dd1a337e1
ALbert12-HELP-ME <kep873291@gmail.com>
2025-05-30 09:08:32 -0400

    qweqweq

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 def home():
     joke_url = "https://v2.jokeapi.dev/joke/Any"
     response = requests.get(joke_url)
-    data = response.json()
+
+    try:
+        data = response.json()
+    except ValueError:
+        return "Error: Could not parse JSON response"
 
     if data["type"] == "single":
         joke = data["joke"]
@@ -22,4 +26,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
